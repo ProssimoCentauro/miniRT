5e3eec9768d7199f01aeda94995a6d1241c790fc
ProssimoCentauro <rik.todaro@gmail.conm>
2025-06-16 19:55:13 +0200

    NORM: norminetted utils files

--- srcs/utils/error_utils.c
+++ srcs/utils/error_utils.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtodaro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/16 19:54:17 by rtodaro           #+#    #+#             */
+/*   Updated: 2025/06/16 19:54:20 by rtodaro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minirt.h"
 
-int		launch_error(int error, char *line1, char *line2, char *line3)
+int	launch_error(int error, char *line1, char *line2, char *line3)
 {
 	print_error(line1, line2, line3);
 	return (error);
@@ -15,7 +27,7 @@ void	print_error(char *line1, char *line2, char *line3)
 	if (line3)
 		ft_putstr_fd(line3, STDERR_FILENO);
 	if (line1 || line2 || line3)
-	write(STDERR_FILENO, "\n", 1);
+		write(STDERR_FILENO, "\n", 1);
 }
 
 void	exit_error(t_renderer *r, char *l1, char *l2, char *l3)
