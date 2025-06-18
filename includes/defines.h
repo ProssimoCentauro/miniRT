d63145411fdf4ba6865d68e7895763dfa116797a
ProssimoCentauro <rik.todaro@gmail.conm>
2025-06-18 20:03:18 +0200

    NORM: norminetted header files

--- includes/defines.h
+++ includes/defines.h
@@ -1,59 +1,73 @@
-#ifndef DEFINES_H
-#define DEFINES_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   defines.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rtodaro <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/18 20:02:34 by rtodaro           #+#    #+#             */
+/*   Updated: 2025/06/18 20:02:34 by rtodaro          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "minirt.h"
+#ifndef DEFINES_H
+# define DEFINES_H
+
+# include "minirt.h"
 
 // Reset
-#define RESET       "\x1b[0m"
+# define RESET "\x1b[0m"
 
 // Bold colors
-#define BOLD_BLACK  "\x1b[1;30m"
-#define BOLD_RED    "\x1b[1;31m"
-#define BOLD_GREEN  "\x1b[1;32m"
-#define BOLD_YELLOW "\x1b[1;33m"
-#define BOLD_BLUE   "\x1b[1;34m"
-#define BOLD_MAGENTA "\x1b[1;35m"
-#define BOLD_CYAN   "\x1b[1;36m"
-#define BOLD_WHITE  "\x1b[1;37m"
+# define BOLD_BLACK "\x1b[1;30m"
+# define BOLD_RED "\x1b[1;31m"
+# define BOLD_GREEN "\x1b[1;32m"
+# define BOLD_YELLOW "\x1b[1;33m"
+# define BOLD_BLUE "\x1b[1;34m"
+# define BOLD_MAGENTA "\x1b[1;35m"
+# define BOLD_CYAN "\x1b[1;36m"
+# define BOLD_WHITE "\x1b[1;37m"
 
-//Errors
-#define MALLOC_ERROR BOLD_RED"MALLOC ERROR"RESET
-#define GENERAL_ERROR BOLD_RED"Error\n"RESET
-#define ARGS_ERROR BOLD_RED"INVALID ARGUMENTS!"RESET
-#define NOT_RT_FILE_ERROR BOLD_RED"NOT .rt or INVALID FILE!"RESET
-#define NON_EXISTENT_FILE_ERROR BOLD_RED"THE INDICATED FILE DOES NOT EXIST!"RESET
-#define FILE_OPENING_ERROR BOLD_RED"ERROR OPENING THE FILE!"RESET
-#define EMPTY_FILE_ERROR BOLD_RED"THE SELECTED FILE IS EMPTY!"RESET
+// Errors (hardcoded colors for 42 norm)
+# define MALLOC_ERROR "\x1b[1;31mMALLOC ERROR\x1b[0m"
+# define GENERAL_ERROR "\x1b[1;31mError\n\x1b[0m"
+# define ARGS_ERROR "\x1b[1;31mINVALID ARGUMENTS!\x1b[0m"
+# define NOT_RT_FILE_ERROR "\x1b[1;31mNOT .rt or INVALID FILE!\x1b[0m"
+# define NON_EXISTENT_FILE_ERROR "\x1b[1;31mTHE INDICATED FILE DOES \
+NOT EXIST!\x1b[0m"
+# define FILE_OPENING_ERROR "\x1b[1;31mERROR OPENING THE FILE!\x1b[0m"
+# define EMPTY_FILE_ERROR "\x1b[1;31mTHE SELECTED FILE IS EMPTY!\x1b[0m"
 
-//Syntax Errors
-#define SYNTAX_ERROR BOLD_RED"Error\nINVALID_SYNTAX!\n"RESET
-#define MISSING_ERROR BOLD_RED "⬤ " RESET "Missing istance of "
-#define TOO_MANY BOLD_RED "⬤ " RESET "Too many istances of "
-#define INVALID_PARAMS BOLD_RED "⬤ " RESET "Invalid number of params for "
-#define COORD_ERROR BOLD_RED "⬤ " RESET "Invalid coords for "
-#define NORMAL_ERROR BOLD_RED "⬤ " RESET "Invalid normal for "
-#define FOV_ERROR BOLD_RED "⬤ " RESET "Invalid fov for "
-#define RATIO_ERROR BOLD_RED "⬤ " RESET "Invalid ratio for "
-#define COLOR_ERROR BOLD_RED "⬤ " RESET "Invalid color for "
-#define DIAMETER_ERROR BOLD_RED "⬤ " RESET "Invalid DIAMETER for "
-#define HEIGHT_ERROR BOLD_RED "⬤ " RESET "Invalid HEIGHT for "
+// Syntax Errors
+# define SYNTAX_ERROR "\x1b[1;31mError\nINVALID_SYNTAX!\n\x1b[0m"
+# define MISSING_ERROR "\x1b[1;31m⬤ \x1b[0mMissing istance of "
+# define TOO_MANY "\x1b[1;31m⬤ \x1b[0mToo many istances of "
+# define INVALID_PARAMS "\x1b[1;31m⬤ \x1b[0mInvalid number of params for "
+# define COORD_ERROR "\x1b[1;31m⬤ \x1b[0mInvalid coords for "
+# define NORMAL_ERROR "\x1b[1;31m⬤ \x1b[0mInvalid normal for "
+# define FOV_ERROR "\x1b[1;31m⬤ \x1b[0mInvalid fov for "
+# define RATIO_ERROR "\x1b[1;31m⬤ \x1b[0mInvalid ratio for "
+# define COLOR_ERROR "\x1b[1;31m⬤ \x1b[0mInvalid color for "
+# define DIAMETER_ERROR "\x1b[1;31m⬤ \x1b[0mInvalid DIAMETER for "
+# define HEIGHT_ERROR "\x1b[1;31m⬤ \x1b[0mInvalid HEIGHT for "
 
-//Objs Messages
-#define CAM_MSG BOLD_CYAN "CAMERA" RESET " object"	
-#define LIGHT_MSG BOLD_GREEN "LIGHT" RESET " object"	
-#define A_LIGHT_MSG BOLD_MAGENTA "AMBIENT LIGHT" RESET " object"	
-#define PL_MSG BOLD_YELLOW "PLANE" RESET " object"	
-#define SP_MSG BOLD_YELLOW "SPHERE" RESET " object"	
-#define CY_MSG BOLD_YELLOW "CYLINDER" RESET " object"	
+// Objs Messages
+# define CAM_MSG "\x1b[1;36mCAMERA\x1b[0m object"
+# define LIGHT_MSG "\x1b[1;32mLIGHT\x1b[0m object"
+# define A_LIGHT_MSG "\x1b[1;35mAMBIENT LIGHT\x1b[0m object"
+# define PL_MSG "\x1b[1;33mPLANE\x1b[0m object"
+# define SP_MSG "\x1b[1;33mSPHERE\x1b[0m object"
+# define CY_MSG "\x1b[1;33mCYLINDER\x1b[0m object"
 
-//Objs Errors
-#define CAM_ERROR BOLD_CYAN "CAMERA" BOLD_RED " object"	
-#define LIGHT_ERROR BOLD_GREEN "LIGHT" BOLD_RED " object"	
-#define A_LIGHT_ERROR BOLD_MAGENTA "AMBIENT LIGHT" BOLD_RED " object"	
-#define PL_ERROR BOLD_YELLOW "PLANE" BOLD_RED " object"	
-#define SP_ERROR BOLD_YELLOW "SPHERE" BOLD_RED " object"	
-#define CY_ERROR BOLD_YELLOW "CYLINDER" BOLD_RED " object"	
+// Objs Errors
+# define CAM_ERROR "\x1b[1;36mCAMERA\x1b[1;31m object"
+# define LIGHT_ERROR "\x1b[1;32mLIGHT\x1b[1;31m object"
+# define A_LIGHT_ERROR "\x1b[1;35mAMBIENT LIGHT\x1b[1;31m object"
+# define PL_ERROR "\x1b[1;33mPLANE\x1b[1;31m object"
+# define SP_ERROR "\x1b[1;33mSPHERE\x1b[1;31m object"
+# define CY_ERROR "\x1b[1;33mCYLINDER\x1b[1;31m object"
 
-//Messages
-#define VALID_FILE_MSG BOLD_GREEN"\nVALID FILE!\n"RESET
+// Messages
+# define VALID_FILE_MSG "\x1b[1;32m\nVALID FILE!\n\x1b[0m"
+
 #endif
