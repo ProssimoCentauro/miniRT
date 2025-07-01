421a8b447938cf440132f7320f738e3a6963b888
Riccardo Emanuele Todaro <rtodaro@e4r10p2.42roma.it>
2025-07-01 18:08:29 +0200

    FEATURE: changed window size

--- srcs/init_funcs/mlx_init.c
+++ srcs/init_funcs/mlx_init.c
@@ -6,15 +6,15 @@
 /*   By: rtodaro <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 21:22:15 by rtodaro           #+#    #+#             */
-/*   Updated: 2025/06/30 21:22:35 by rtodaro          ###   ########.fr       */
+/*   Updated: 2025/07/01 15:58:53 by rtodaro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-static void	init_image(void	*mlx, t_image *image)
+static void	init_image(t_mlx *mlx, t_image *image)
 {
-	image->img = mlx_new_image(mlx, 500, 500);
+	image->img = mlx_new_image(mlx->mlx_instance, mlx->width, mlx->height);
 	image->addr = mlx_get_data_addr(
 			image->img,
 			&(image)->bits_per_pixel,
@@ -25,8 +25,11 @@ static void	init_image(void	*mlx, t_image *image)
 void	init_mlx(t_mlx	*mlx)
 {
 	mlx->mlx_instance = mlx_init();
-	mlx->window = mlx_new_window(mlx->mlx_instance, 500, 500, "miniRT!");
-	mlx->width = 500;
-	mlx->height = 500;
-	init_image(mlx->mlx_instance, &(mlx)->image);
+	mlx_get_screen_size(mlx->mlx_instance, &(mlx)->width, &(mlx)->height);
+	mlx->width = mlx->width / 2;
+	mlx->height = mlx->height / 2;
+	mlx->window = mlx_new_window(mlx->mlx_instance, mlx->width, mlx->height, "miniRT!");
+	//mlx->width = 500;
+	//mlx->height = 500;
+	init_image(mlx, &(mlx)->image);
 }
