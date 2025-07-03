f5b6c59d819097a63bc02c1eba8e71cdce85aea5
iacopo <iacopo.brunialti@gmail.com>
2025-07-03 12:58:48 +0200

    FEATURE: renamed and correct some typo

--- libft/header_files/vector.h
+++ libft/header_files/vector.h
@@ -29,10 +29,11 @@ t_vector	vector_add4(t_vector vec1, t_vector vec2, t_vector vec3, t_vector vec4)
 t_vector	vector_sub(t_vector vec1, t_vector vec2);
 double		vector_dot(t_vector vec1, t_vector vec2);
 t_vector	vector_cross(t_vector vec1, t_vector vec2);
-double		vector_length(t_vector vec);
-double		vector_length_squared(t_vector vec);
+double		vector_lenght(t_vector vec);
+double		vector_lenght_squared(t_vector vec);
 t_vector	vector_normalize(t_vector vec);
 t_vector	vector_project(t_vector vec1, t_vector vec2);
 t_vector    vector_scale(t_vector vec, double scale);
+t_vector    vector_invert(t_vector vec);
 
 #endif
