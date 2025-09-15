4ce4b0c65d81ed2f175ced81baab72cd8ffab55b
ProssimoCentauro <rik.todaro@gmail.conm>
2025-09-15 21:15:25 +0200

    FEATURE: added rotation func, obj can be rotated by pressing keys 7,8,9,0

--- srcs/handlers/events_handlers.c
+++ srcs/handlers/events_handlers.c
@@ -6,7 +6,7 @@
 /*   By: rtodaro <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 21:20:44 by rtodaro           #+#    #+#             */
-/*   Updated: 2025/09/15 19:55:05 by rtodaro          ###   ########.fr       */
+/*   Updated: 2025/09/15 21:14:33 by rtodaro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,46 @@ t_vector	*get_obj_coord(t_object *obj)
 	return (NULL);
 }
 
+t_vector *get_obj_normal(t_object *obj)
+{
+	if (!obj)
+		return (NULL);
+	if (obj->type == PLANE)
+		return (&obj->figure.plane.normal);
+	if (obj->type == CYLINDER)
+		return (&obj->figure.cylinder.normal);
+	if (obj->type == CONE)
+		return (&obj->figure.cone.normal);
+	return (NULL);
+}
+
+
+t_vector rotate_vector_axis(t_vector v, t_vector axis, double angle)
+{
+    t_vector res;
+    double cos_t = cos(angle);
+    double sin_t = sin(angle);
+
+    axis = vector_normalize(axis); // importantissimo!
+
+    res = vector_add(
+              vector_scale(v, cos_t),
+              vector_add(
+                  vector_scale(vector_cross(axis, v), sin_t),
+                  vector_scale(axis, vector_dot(axis, v) * (1 - cos_t))
+              )
+          );
+    return res;
+}
+
+
 void	move_selected_obj(int key, t_renderer *r)
 {
 	t_vector	*obj_coord;
+	t_vector	*obj_normal;
 
-	obj_coord = get_obj_coord(r->scene->selected_obj);
+	obj_coord	= get_obj_coord(r->scene->selected_obj);
+	obj_normal  = get_obj_normal(r->scene->selected_obj);
 	if (key == 49)
 		*obj_coord = vector_sub(*obj_coord, r->scene->cam->right);
 	else if (key == 50)
@@ -44,6 +79,14 @@ void	move_selected_obj(int key, t_renderer *r)
 		*obj_coord = vector_add(*obj_coord, r->scene->cam->orientation);
 	else if (key == 54)
 		*obj_coord = vector_sub(*obj_coord, r->scene->cam->orientation);
+	else if (key == 55 && obj_normal)
+		*obj_normal = rotate_vector_axis(*obj_normal, r->scene->cam->up, M_PI / 36);
+	else if (key == 56 && obj_normal)
+		*obj_normal = rotate_vector_axis(*obj_normal, r->scene->cam->up, M_PI / -36);
+	else if (key == 57 && obj_normal)
+		*obj_normal = rotate_vector_axis(*obj_normal, r->scene->cam->right, M_PI / -36);
+	else if (key == 48 && obj_normal)
+		*obj_normal = rotate_vector_axis(*obj_normal, r->scene->cam->right, M_PI / 36);
 }
 
 int	exit_handler(t_renderer *r)
@@ -58,7 +101,12 @@ int	events_handler(int key, t_renderer *r)
 	printf("key: %d\n", key);
 	if ((char)key == '\033')
 		exit_handler(r);
-	if (key >= 49 && key <= 54 && r->scene->selected_obj)
+	if (key >= 48 && key <= 57 && r->scene->selected_obj)
+	{
+		move_selected_obj(key, r);
+		render_scene(r);
+	}
+	if (key >= 65361 && key <= 65364 && r->scene->selected_obj)
 	{
 		move_selected_obj(key, r);
 		render_scene(r);
